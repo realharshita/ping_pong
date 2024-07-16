3790fd39ef0966f0ec682ca58fd7bc40449724b5
realharshita <slayyharshita@gmail.com>
2024-07-16 19:04:27 +0530

    pong

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ CANVAS_HEIGHT = 600
 PAD_WIDTH = 10
 PAD_HEIGHT = 80
 BALL_SIZE = 20
+BALL_SPEED = 5  # Initial speed of the ball
 
 window = tk.Tk()
 window.title("Ping Pong Game")
@@ -25,6 +26,28 @@ ball = canvas.create_oval(CANVAS_WIDTH // 2 - BALL_SIZE // 2, CANVAS_HEIGHT // 2
 
 center_line = canvas.create_line(CANVAS_WIDTH // 2, 0, CANVAS_WIDTH // 2, CANVAS_HEIGHT, fill="white", dash=(15, 10))
 
+ball_start_x = CANVAS_WIDTH // 2
+ball_start_y = CANVAS_HEIGHT // 2
+ball_dx = BALL_SPEED
+ball_dy = BALL_SPEED
+
+def update_ball_position():
+    global ball_start_x, ball_start_y, ball_dx, ball_dy
+    
+    canvas.move(ball, ball_dx, ball_dy)
+    
+    ball_pos = canvas.coords(ball)
+    ball_left = ball_pos[0]
+    ball_top = ball_pos[1]
+    ball_right = ball_pos[2]
+    ball_bottom = ball_pos[3]
+    
+    if ball_top <= 0 or ball_bottom >= CANVAS_HEIGHT:
+        ball_dy = -ball_dy
+    
+    if canvas.coords(ball)[2] >= canvas.coords(paddle2)[0] and canvas.coords(ball)[3] <= canvas.coords(paddle2)[3] and canvas.coords(ball)[1] >= canvas.coords(paddle2)[1]:
+        canvas.move(ball, ball_dx, ball_dy)
+
 def move_paddle(event):
     if event.keysym == "w":
         canvas.move(paddle1, 0, -20)
@@ -40,4 +63,9 @@ canvas.bind_all("<KeyPress-s>", move_paddle)
 canvas.bind_all("<KeyPress-Up>", move_paddle)
 canvas.bind_all("<KeyPress-Down>", move_paddle)
 
+def game_loop():
+    update_ball_position()
+    window.after(30, game_loop)
+
+game_loop()
 window.mainloop()
